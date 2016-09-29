c60adf5ed7d8c8551d787cd994330a355fb1f4b1
EdmilDM <edmildm@gmail.com>
2016-09-29 02:33:37 -0400

    Agregando algunos beans.

---
+++ src/main/java/src/java/Beans/CarritoBean.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.java.Beans;
+
+import java.util.List;
+import modelo.Usuario;
+import src.java.modelo.Producto;
+
+/**
+ *
+ * @author edmildimassimo
+ */
+public class CarritoBean {
+    private List<Producto> productos;
+    private int id;
+    private String nombre;
+    private String description;
+    private int cantidad;
+    private double precio;
+    private Usuario usuario;
+
+    /**
+     * @return the productos
+     */
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    /**
+     * @param productos the productos to set
+     */
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public String agregarProducto(){
+        Producto nuevo = new Producto();
+        nuevo.setCantidad(cantidad);
+        nuevo.setDescription(description);
+        nuevo.setNombre(nombre);
+        nuevo.setPrecio(precio);
+        productos.add(nuevo);
+        return null;
+    }
+}
