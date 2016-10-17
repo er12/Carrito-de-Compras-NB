4b84e72821ac16f804eb32bd019dc8eddf5c7cbe
Ernesto Rodríguez <ernestorod27@gmail.com>
2016-10-17 04:27:47 -0400

    imagenes B@#$%^s

--- src/main/java/helper/Tienda.java
+++ src/main/java/helper/Tienda.java
@@ -1,13 +1,11 @@
 package helper;
 
-
 import java.util.ArrayList;
-import javax.ejb.Stateless;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.bean.ManagedBean;
-import modelo.Producto;
+import src.java.modelo.Producto;
 import java.io.Serializable;
-import java.util.List;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
+import src.java.modelo.Comentario;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,12 +22,9 @@ public class Tienda implements Serializable {
     
     private static Tienda instancia = null;
     private static int ids = 0;
-    private static List<Producto> Carrito;
-    private static List<Producto> productosDisponibles;
-    private static List<Producto> Comprados;
-
-    
-            
+    private static ArrayList<Producto> Carrito;
+    private static ArrayList<Producto> productosDisponibles;
+    private static ArrayList<Producto> Comprados;  
     
     private Tienda() {
         
@@ -46,39 +41,54 @@ public class Tienda implements Serializable {
         return instancia;
     }
     
-    public void insertarProducto(String nombre,String description,int cantidad,double precio)
-    {
-        productosDisponibles.add(new Producto(ids,nombre,description,cantidad,precio));
+    public void insertarProducto(String nombre,String description,int cantidad,double precio,UploadedFile uf){
+        Producto p = new Producto(ids,nombre,description,cantidad,precio, uf);
+        p.setUpFile(uf);
+        
+        productosDisponibles.add(p);
         ids++;
-        
-        
+    }
+    
+    public void agregarComentario(int prodID, Comentario c){
+        for(Producto p : productosDisponibles ){
+            if( prodID == p.getId()){
+                p.getComentarios().add(c);
+                return;
+            }
+        }
     }
 
-    public List<Producto> getCarrito() {
+    public ArrayList<Producto> getCarrito() {
         return Carrito;
     }
 
-    public void setCarrito(List<Producto> Carrito) {
+    public void setCarrito(ArrayList<Producto> Carrito) {
         this.Carrito = Carrito;
     }
 
-    public List<Producto> getProductosDisponibles() {
+    public ArrayList<Producto> getProductosDisponibles() {
         return productosDisponibles;
     }
 
-    public void setProductosDisponibles(List<Producto> productosDisponibles) {
+    public void setProductosDisponibles(ArrayList<Producto> productosDisponibles) {
         this.productosDisponibles = productosDisponibles;
     }
 
-    public List<Producto> getComprados() {
+    public ArrayList<Producto> getComprados() {
         return Comprados;
     }
 
-    public void setComprados(List<Producto> Comprados) {
+    public void setComprados(ArrayList<Producto> Comprados) {
         this.Comprados = Comprados;
     }
-    
-    
+
+    public Tienda getInstancia() {
+        return instancia;
+    }
+
+    public void setInstancia(Tienda instancia) {
+        Tienda.instancia = instancia;
+    }
     
     
     
